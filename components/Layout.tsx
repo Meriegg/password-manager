e7e0b7dcfce96d43c3de38f3bdcdfa68d4890c2d
Meriegg <ya.boybeats1234@gmail.com>
2022-04-06 20:49:30 +0300

    Added title to page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Head from "next/head";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
+      <Head>
+        <title>Password Manager | Store Your Passwords</title>
+      </Head>
       <>{children}</>
     </div>
   );
